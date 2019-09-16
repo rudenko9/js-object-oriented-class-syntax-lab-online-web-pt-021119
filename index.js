4ff2f7673968558a6f3756be5cb3ac2f61a2f412
Nataliya R <n.rudenko@gmail.com>
2019-09-16 23:29:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -24,7 +24,9 @@ class BoardMember {
      return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
    };
  
- class Ceo extends BoardMember
+ class Ceo extends BoardMember {
+   
+ }
  
  
  
